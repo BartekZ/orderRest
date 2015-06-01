a4a26058c9abdc329a12cf5ff8c4db6a70557d1b
BartZ <bartz>
2015-06-01 15:30:00 +0200

    mod2

---
+++ orderRest/src/SingleOrderServiceTest.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import pl.proacem.dao.HbnSingleOrderDao;
+import pl.proacem.model.SingleOrder;
+import pl.proacem.service.SingleOrderService;
+
+
+public class SingleOrderServiceTest {
+
+	
+	@Test
+	public void test() {
+		List<SingleOrder> list = new ArrayList<SingleOrder>();
+		SingleOrderService service = new SingleOrderService();
+		list = service.findOfferNumber("452");
+		System.out.println(list);
+		//assertEquals((1+1), 2);
+	}
+
+	@Test
+	public void HbnSingleOrderDaoTest(){
+		List<SingleOrder> list = new ArrayList<SingleOrder>();
+		HbnSingleOrderDao hbnSingleOrderDao = new HbnSingleOrderDao();
+		list = hbnSingleOrderDao.findByOrderNumber("452");
+		System.out.println(list);
+		
+		
+	}
+}
